8a07a2fac662882c98960dd07543b5b22675659b
rambabukella <rambabukella@users.noreply.github.com>
2023-04-27 19:00:24 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,5 @@
 node default {
-  file {'/root/README':
-    ensure => file,
-    content => 'This is a readme',
-    owner => 'root',
-    }
-
 }
+node 'puppetmaster.localdomain.com' {
+  include role::master_server
+  }
