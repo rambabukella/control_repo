66354197491c92c75111da1bd61601c1cdb61693
rambabukella <rambabukella@users.noreply.github.com>
2023-04-27 19:16:23 +0530

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,5 @@
 class profile::agent_nodes {
   include docker
-  docker::node {'qa1-web.localadmin.vm'}
-  docker::node {'qa1-db:wq.localadmin.vm'}
+  docker::node {'qa1-web.localadmin.vm':}
+  docker::node {'qa1-db:wq.localadmin.vm':}
 }
